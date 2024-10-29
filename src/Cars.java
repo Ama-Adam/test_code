8a32367d5231d558ff67c01ec0c90da9eeec8ab3
Ruth Arhin <wrutharhin@gmail.com>
2024-10-29 09:34:40 +0000

    testing git and github

--- src/Cars.java
+++ src/Cars.java
@@ -4,7 +4,7 @@ public class Cars {
     int year = 2024;
     String color = "Blue";
     double price = 50000.00;
-    String owner = "ruth";
+    String owners = "ruth";
 
     void drive(){
         System.out.println("You drive the car");
